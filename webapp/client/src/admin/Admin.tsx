aa79b281f3bb55864d04214cbd29206a22638b63
Petre Hosliag <petre.hosliag@sm-services.es>
2025-05-27 10:47:31 +0200

    DEV - admin

--- webapp/client/src/admin/Admin.tsx
+++ webapp/client/src/admin/Admin.tsx
@@ -1,6 +1,34 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import { getTrxSuccess, getTrxError } from "../features/adminSlice";
 
 const Admin = () => {
-  return <div><h1>Yo soy ADMIN</h1></div>;
+
+  const dispatch = useAppDispatch();
+  const { trxSuccess, trxError } = useAppSelector(state => state.admin);
+
+  useEffect(() => {
+    dispatch(getTrxSuccess());
+    dispatch(getTrxError());
+  }, [dispatch]);
+
+  return (<div>
+    <h1>Yo soy ADMIN</h1>
+    <div>
+      <h2>Transacciones exitosas</h2>
+      <ul>
+        {trxSuccess.map(trx => <li key={trx.id}>{trx.id}</li>)}
+      </ul>
+    </div>
+    <div>
+      <h2>Transacciones fallidas</h2>
+      <ul>
+        {trxError.map(trx => <li key={trx.id}>{trx.id}</li>)}
+      </ul>
+    </div>
+    
+    </div>);
 };
+
 
 export default Admin;
--- webapp/client/src/app/store.ts
+++ webapp/client/src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import bondReducer from '../features/bondSlice';
 import userReducer from '../features/userSlice';
+import adminReducer from '../features/adminSlice';
 
 export const store = configureStore({
     reducer: {
         bond: bondReducer,
-        user: userReducer
+        user: userReducer,
+        admin: adminReducer
     },
 })
 
